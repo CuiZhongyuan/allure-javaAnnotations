7213feb09a9317ee6ea258eb93b4e2814dc0c1c0
rootczy <365450906@qq.com>
2020-09-29 15:43:55 +0800

    测试用例入口

---
+++ src/test/java/EnterCases.java
@@ -0,0 +1,16 @@
+import com.allurejava.util.BaseTestngInit;
+import org.testng.annotations.Test;
+
+public class EnterCases {
+    BaseTestngInit baseTestngInit = new BaseTestngInit();
+
+    /**
+     * 驱动存放路径(小程序的webdriver) target/classes/chromedriver/chromedriver.exe
+     */
+    private String xmlPath=this.getClass().getClassLoader().getResource("testng.xml").getPath();
+
+    @Test
+    public void runCase(){
+        baseTestngInit.baseTestngInitXml(xmlPath);
+    }
+}
